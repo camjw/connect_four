b585d25b82ce92186c69df7eb13d3741ec2e30e9
Cameron Whitehead <camjw119@gmail.com>
2018-08-28 14:48:00 +0100

    Refactored board slightly

--- board.rb
+++ board.rb
@@ -1,3 +1,6 @@
+# This class contains the board for Connect Four and checks when whether the
+# game has been won and if moves are valid.
+
 class Board
 
   attr_accessor :board_state, :played_moves
@@ -51,24 +54,39 @@ class Board
 
   def game_won?(symbol)
 
-    four_in_a_row = symbol * 4
+    return horizontal_win?(symbol) || vertical_win?(symbol) ||
+      diagonal_win?(symbol)
 
-    # First we check the columns, then the rows, then the diagonals
+  end
 
-    @played_moves.keys.each { |i|
-      col = @played_moves[i.to_s]
-      if col[3] == symbol && col.join("").include?(four_in_a_row)
+  def vertical_win?(symbol)
+
+    @played_moves.keys.each { | index |
+      col = @played_moves[index.to_s]
+      if col[3] == symbol && col.join("").include?(symbol * 4)
         return true
       end
     }
 
-    6.times { |i|
-    midpoint = 4 + (9 * i)
+    return false
+
+  end
+
+  def horizontal_win?(symbol)
+
+    6.times { | index |
+    midpoint = 4 + (9 * index)
     if @board_state[midpoint] != "-" &&
-      @board_state[midpoint - 3..midpoint + 3].join("").include?(four_in_a_row)
+      @board_state[midpoint - 3..midpoint + 3].join("").include?(symbol * 4)
         return true
     end
     }
+
+    return false
+
+  end
+
+  def diagonal_win?(symbol)
 
     # Diagonal checking happens here
 
@@ -79,20 +97,17 @@ class Board
     ]
 
     diag_indices.each { | start_index |
-      puts start_index
-      puts @board_state
       down_diag = @board_state[start_index] + @board_state[start_index + 10] +
         @board_state[start_index + 20] + @board_state[start_index + 30]
 
       up_diag = @board_state[start_index + 3] + @board_state[start_index + 11] +
         @board_state[start_index + 19] + @board_state[start_index + 27]
 
-      if up_diag == four_in_a_row || down_diag == four_in_a_row
+      if up_diag == symbol * 4 || down_diag == symbol * 4
         return true
       end
 
     }
-
 
     return false
 
