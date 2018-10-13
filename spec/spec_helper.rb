3d9dace63c1b6f4e572de1fb87dc9bed944ccc6d
Cameron Whitehead <camjw119@gmail.com>
2018-10-13 19:35:38 +0100

    Updated spec helper again

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,19 +1,17 @@
-# frozen_string_literal: true
-
 require 'simplecov'
-require 'simplecov-console'
 SimpleCov.start
 
 require 'codecov'
 SimpleCov.formatter = SimpleCov::Formatter::Codecov
-
+#
 # require 'rake'
+# require 'simplecov'
+# require 'simplecov-console'
 # require 'pry'
-
-SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([
-                                                                 SimpleCov::Formatter::Console,
-                                                                 SimpleCov::Formatter::HTMLFormatter
-                                                               ])
-SimpleCov.start
-
-ENV['TEST_ENV'] = 'test'
+#
+#
+# SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([
+#   SimpleCov::Formatter::Console,
+#   SimpleCov::Formatter::HTMLFormatter
+# ])
+# SimpleCov.start
