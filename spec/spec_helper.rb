7eb66edb64dc7594ba230c5c5b62bb65fa0fda1a
Cameron Whitehead <camjw119@gmail.com>
2018-10-13 19:43:40 +0100

    switched branch

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,19 +1,19 @@
-# frozen_string_literal: true
-
 require 'simplecov'
-require 'simplecov-console'
 SimpleCov.start
 
-# require 'codecov'
-# SimpleCov.formatter = SimpleCov::Formatter::Codecov
-
-# require 'rake'
-# require 'pry'
-
-SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([
-                                                                 SimpleCov::Formatter::Console,
-                                                                 SimpleCov::Formatter::HTMLFormatter
-                                                               ])
-SimpleCov.start
+require 'codecov'
+SimpleCov.formatter = SimpleCov::Formatter::Codecov
 
 ENV['TEST_ENV'] = 'test'
+
+# require 'rake'
+# require 'simplecov'
+# require 'simplecov-console'
+# require 'pry'
+#
+#
+# SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([
+#   SimpleCov::Formatter::Console,
+#   SimpleCov::Formatter::HTMLFormatter
+# ])
+# SimpleCov.start
